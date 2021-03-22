28316ee252142b301f59a7b347826f1ed962dcf9
Conrad Stevens <conrad.stevens@gmail.com>
2021-03-21 20:42:39 -0400

    Ai Sanke complete

--- snake-pygame/agent.py
+++ snake-pygame/agent.py
@@ -1,16 +1,15 @@
 import torch
 import random
 import numpy as np
+from collections import deque
 from game import SnakeGameAI, Direction, Point
-from helper import plot
-
-from game import SnakeGameAI, Direction, Point
-from _collections import deque
 from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
+
 
 class Agent:
 
@@ -65,6 +64,7 @@ class Agent:
             game.food.y < game.head.y,  # food up
             game.food.y > game.head.y  # food down
         ]
+        return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
@@ -82,9 +82,9 @@ class Agent:
 
     def get_action(self, state):
         # random moves: tradeoff exploration / exploration
-        self.epsilon = 80 - self.n_games  # Temporary Hard code
+        self.epsilon = 1 - self.n_games  # Temporary Hard code
         final_move = [0,0,0]
-        
+
         if random.randint(0, 200) < self.epsilon:
             move = random.randint(0, 2)
             final_move[move] = 1
@@ -93,7 +93,9 @@ class Agent:
             prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
+
         return final_move
+
 
 def train():
     plot_scores = []
@@ -124,7 +126,7 @@ def train():
             # train long memory -- trains again on all previous moves
             game.reset()
             agent.n_games += 1
-            agent.train_long()
+            agent.train_long_memory()
 
             if score > record:
                 record = score
@@ -138,5 +140,6 @@ def train():
             plot_mean_scores.append(mean_score)
             plot(plot_scores, plot_mean_scores)
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     train()
